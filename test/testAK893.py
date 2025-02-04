ed438e937789327961f028c284697c30e3dd789d
dustinteng <dustinteng12@gmail.com>
2025-02-03 17:26:04 -0800

    removing test folder

--- test/testAK893.py
+++
@@ -1,8 +0,0 @@
-import smbus2
-
-bus = smbus2.SMBus(1)
-address_ak = 0x0C  # AK8963 address
-
-# Read the WHO_AM_I register
-who_am_i_ak8963 = bus.read_byte_data(address_ak, 0x00)
-print(f"AK8963 WHO_AM_I register: {hex(who_am_i_ak8963)}")
