49ee5aa88b84c16333e2a4cadd820668105a51f7
KieranPereira <kieranpereira@hotmail.com>
2025-03-10 22:50:14 -0700

    updating witmotionmag/setup.py

--- src/sensors/witmotionmag/setup.py
+++ src/sensors/witmotionmag/setup.py
@@ -5,14 +5,10 @@ package_name = 'witmotionmag'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(where='src') + ['lib', 'lib.data_processor', 'lib.data_processor.roles', 'lib.data_processor.interface', 'lib.protocol_resolver.roles', 'lib.protocol_resolver.interface'],
-    package_dir={
-        '': 'src',
-        'lib': 'lib'  # This makes Python recognize it as a package
-    },
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -20,11 +16,11 @@ setup(
     maintainer='boat',
     maintainer_email='dustinteng12@gmail.com',
     description='ROS2 package for WitMotionMag sensor',
-    license='TODO: License declaration',  # Replace with your license
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'witmotionmag_node = witmotionmag.witmotionmag_node:main'  # Check for the correct path to your main function
+            'witmotionmag_node = witmotionmag.witmotionmag_node:main'
         ],
     },
 )
